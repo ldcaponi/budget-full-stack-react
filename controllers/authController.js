b57db4176c9ea8777a8ec6b78e49e373c7fe241b
Lou <ldcaponi@gmail.com>
2019-01-08 10:38:31 -0500

    Added hashed passwords.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const pool = require("../db");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const login = (req, res) => {
   const { email, password } = req.body;
@@ -13,18 +14,18 @@ const login = (req, res) => {
       }
 
       const rows = results.rows;
-      console.log("ROWS", rows);
       if (rows && rows.length > 0) {
         const user = rows[0];
-        console.log("USER", user);
+
         if (user) {
-          if (password === user.password) {
+          bcrypt.compare(password, user.password, (err, success) => {
+            if (err || !success) {
+              return res.status(401).send({ message: "Could not log in" });
+            }
             const payload = { id: user.id };
             const token = jwt.sign(payload, process.env.APP_SECRET);
             return res.send({ message: "Success!", token });
-          } else {
-            return res.status(401).send({ message: "Could not log in" });
-          }
+          });
         }
       } else {
         return res.status(500).send({ message: "Could not find user" });
@@ -39,21 +40,26 @@ const signUp = (req, res) => {
     return res.status(403).message("Missing information");
   }
 
-  pool.query(
-    "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
-    [name, email, password],
-    (error, results) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send("Error with query");
-      }
-
-      const newUser = results.rows[0];
-      const payload = { id: newUser.id };
-      const token = jwt.sign(payload, process.env.APP_SECRET);
-      return res.json({ message: "Success!", token });
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).send({ message: "Issue hashing password" });
     }
-  );
+    pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, hash],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).send("Error with query");
+        }
+
+        const newUser = results.rows[0];
+        const payload = { id: newUser.id };
+        const token = jwt.sign(payload, process.env.APP_SECRET);
+        return res.json({ message: "Success!", token });
+      }
+    );
+  });
 };
 
 const me = (req, res) => {
